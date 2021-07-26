447b3123cd4a3bf14a37cbc6eff1d5da45bb53b6
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-07-26 17:55:42 -0400

    commit tree f4a33f210c079329df8a325cdaa95fd9a3036fb5

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+  
+  handleClick = (event) => {
+    const xCoord = event.clientX
+    const yCoord = event.clientY
+
+    this.props.onReceiveCoordinates([xCoord, yCoord])
+  }
+  render(){
+    return (
+      <button onClick={this.handleClick}></button>
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+
+  handleClick = (event) => {
+    const savedEvent = event
+
+    setTimeout(this.props.onDelayedClick, this.props.delay)
+  }
+
+  render(){
+    return(
+      <button onClick={handleClick}></button>
+    )
+  }
+}
